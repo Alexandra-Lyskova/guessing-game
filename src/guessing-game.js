d9dfef67273b1640bd0c53979e5419165ea2c845
Alexandra_Lyskova <alexandra.lyskova@gmail.com>
2017-02-05 20:40:57 +0300

    guessing-game/implement-guessingGame-class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor()
+      {
+       this.left = 0;
+       this.right = 0;
+      }
 
     setRange(min, max) {
-
+      this.left = min;
+      this.right = max;
     }
 
     guess() {
-
+      return Math.ceil(this.left + (this.right - this.left)/2);
     }
 
     lower() {
-
+      this.right = Math.ceil(this.left + (this.right - this.left)/2);
     }
 
     greater() {
-
+      this.left = Math.ceil(this.left + (this.right - this.left)/2);
     }
-}
-
+  }
 module.exports = GuessingGame;
